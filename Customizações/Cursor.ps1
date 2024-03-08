b3a7eb23b054c718b3418b349b7f64b587a91039
gguilhermepamplona <guilherme.pamplonaa@gmail.com>
2024-03-08 17:42:49 -0300

    Ajustes

--- Customizações/Cursor.ps1
+++ Customizações/Cursor.ps1
@@ -1,8 +1,23 @@
-$LinkCursor = "https://drive.google.com/uc?export=download&id=1vtIVylHIiemYzkhRa6rYSvfUXbzV7ySj"
+$LinkCursor = "https://drive.google.com/uc?export=download&id=1bwRha4AnjOWcm1PHhVMHnx69KknQIGsl"
+$DirTemp = Join-Path -Path $env:USERPROFILE -ChildPath "AppData\Local\Temp\Cursor\"
 
-if(-not(Test-Path -Path "C:\Windows\Web\Cursor")){
-    New-Item "C:\Windows\Web\Cursor" -ItemType Directory
+if(-not(Test-Path -Path $DirTemp)){
+    New-Item $DirTemp -ItemType Directory
 }
-$CaminhoArquivo = "C:\Windows\Web\Cursor\MacOS Cursor Dark"
+$CaminhoArquivo = Join-Path -Path $DirTemp "\MacOS Cursor Dark.zip"
 
 Invoke-WebRequest -Uri $LinkCursor -OutFile $CaminhoArquivo
+Expand-Archive -Path $CaminhoArquivo -DestinationPath (Split-Path $CaminhoArquivo) -Force
+
+$CaminhoInstall = Join-Path -Path $DirTemp "Install.inf"
+if (Test-Path $CaminhoInstall) {
+$ComandoInstall = "rundll32.exe"
+$arg = "setupapi.dll,InstallHinfSection DefaultInstall 132 $($CaminhoInstall -replace '\\', '\\')"
+Start-Process -FilePath $ComandoInstall -ArgumentList $arg -Wait -Verb RunAs
+} else {
+    Write-Host "O arquivo .inf não foi encontrado."
+}
+
+Start-Process "control.exe" -ArgumentList "main.cpl"
+
+Remove-Item -Path $DirTemp -Recurse -Force
---
+++ Customizações/Terminal.ps1
@@ -0,0 +1,5 @@
+$DiretorioConfig = "C:\Users\$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+$ArquivoConfig = Join-Path $DiretorioConfig "settings.json"
+$LinkConfig = "https://drive.google.com/uc?export=download&id=1YBH7WhIe30oopzYIdeDFar0opst_3--U"
+
+Invoke-WebRequest -Uri $LinkConfig -OutFile $ArquivoConfig
