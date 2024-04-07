dc36ac67040d3510062d4b864c4c747515c3783b
gguilhermepamplona <guilherme.pamplonaa@gmail.com>
2024-04-07 05:36:25 +0000

    Ajustes

--- Customizações/Terminal.ps1
+++ Customizações/Terminal.ps1
@@ -1,5 +1,5 @@
 function Terminal {
-$DiretorioConfig = "C:\Users\$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+$DiretorioConfig = "$env:USERPROFILE\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
 $ArquivoConfig = Join-Path $DiretorioConfig "settings.json"
 $LinkConfig = "https://drive.google.com/uc?export=download&id=1YBH7WhIe30oopzYIdeDFar0opst_3--U"
 
