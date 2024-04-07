67ca0321fd480b1a59584b170a925dfc9a80c50f
gguilhermepamplona <guilherme.pamplonaa@gmail.com>
2024-04-07 16:17:11 +0000

    Ajustes

--- Testes.ps1
+++ Testes.ps1
@@ -1,39 +1,37 @@
 Import-Module -Name ps-menu -Force
 
 $global:opcaoselec = @{}
-$global:nome = "teste"
 
 function OpcoesMenu([array]$opcoes) {
-	$result = Menu -menuItems ($opcoes.o)
+    [string]$result = Menu -menuItems ($opcoes.o)
     $resultnome = $opcoes | Where-Object {$_.o -eq $result}
-    if ($result -like "x ") {
-        $result2 = $result.Replace("x ", "")
+    $teste = $resultnome.o
+    $OpcaoAtual = Get-Variable -Scope Global | Where-Object { $_.Value -eq "$result" -and $_.Value -isnot [bool]}
+    if ($result -like "x *") {
+        $result = $result.Replace("x ", "")
     }
     if ($resultnome.ms) {
-        if ($global:opcaoselec.$result -ne "1" -or $global:opcaoselec.$result2 -ne "1") {
+        if ($global:opcaoselec[$result] -ne "1") {
+            ${global:$OpcaoAtual.Name} = "x " + $OpcaoAtual.Name
             $global:opcaoselec["$result"] = "1"
-            $global:nome = "x " + $global:nome
-        } elseif ($global:opcaoselec.$result -eq "1") {
-            $global:opcaoselec["$global:nome"] = "0"
-            $global:nome.Replace("x ", "")
+        } elseif ($global:opcaoselec[$result] -eq "1") {
+            $OpcaoAtual.Name = $result ; $z = $opcoes | Where-Object {$_.o -eq "$teste"} ; $z.o = $result
+            $global:opcaoselec["$result"] = "0"
         }
     }
     & ($opcoes | Where-Object {$_.o -eq $result}).a
 }
 
 function testemenu {
+    $global:testemenuSistema = "Sistema"
+    $global:testemenuAtivacoes = "Ativações"
     $global:OpcoesMenu = @(
-		@{o = $global:nome ; a = {Clear-Host ; testemenu} ; ms = $true},
-		@{o = "Ativações" ; a = {MenuAtivacoes}},
+		@{o = $global:testemenuSistema ; a = {Clear-Host ; testemenu} ; ms = $true},
+		@{o = $global:testemenuAtivacoes ; a = {Clear-Host ; testemenu} ; ms = $true},
 		@{o = "Customizações" ; a = {MenuCustomizacoes}},
 		@{o = "Sair" ; a = {Sair}}
 	) ; $OpcoesArray = $OpcoesMenu | ForEach-Object {@{o = $_.o ; a = $_.a ; ms = $_.ms}}
 	OpcoesMenu -opcoes $OpcoesArray
 }
 
-function primeiro {
-    $global:nome = "Sistema"
-    testemenu
-}
-
-primeiro
+testemenu
