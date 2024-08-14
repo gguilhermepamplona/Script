a34b1e4990cdddfcd5bdf8515f0c2123f3b0593c
gguilhermepamplona <guilherme.pamplonaa@gmail.com>
2024-08-13 21:30:45 -0300

    Ajustes

---
+++ Sistema/Opções sem categoria.ps1
@@ -0,0 +1,6 @@
+# WinAeroTweaker
+#
+# Disable "look for an app in the store" notification
+# Disable "you have new apps that can open this type of file"
+# Show seconds on taskbar clock
+# Wallpaper quality
